f8d51a4b914ce05038ff9237554a95a8baefbd01
popularkoju <dev.popular72@gmail.com>
2026-08-16 20:34:55 +0545

    copy pasted missed class codes

--- jsModules/index.js
+++ jsModules/index.js
@@ -1,4 +1,44 @@
 
-import chalk from 'chalk';
+
+
+
+
+// import * as check from "./cart.js";
+
+// console.log(check)
+
+import { addProduct, removeProduct, isCartEmpty } from './cart.js'
+import chalk from 'chalk'
+
+// const { addProduct } = require('./cart.js')
+// const { checkout } = require('./checkout.js')
+// //Application code
+// const cart = [];
+
+// addProduct(cart, {
+//   name: "Book",
+//   price: 20
+// });
+
+// addProduct(cart, {
+//   name: "Pen",
+//   price: 5
+// });
+
+// console.log(cart)
+
+// if (true) {
+//   async function importData() {
+//     const modules =await import('./cart.js')
+//     console.log(modules)
+//   }
+//   importData()
+// }
+
+
+// checkout(cart);
+
+
+
 console.log(chalk.bgRed("hello world"))
 
